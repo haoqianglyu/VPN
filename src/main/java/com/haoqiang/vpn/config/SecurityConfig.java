2e39ac0acb19de057f6c263aebbec3ef93e7bc7a
haoqianglyu <haoqianglyu@gmail.com>
2019-06-19 12:16:37 -0400

    add entrypoint

--- src/main/java/com/haoqiang/vpn/config/SecurityConfig.java
+++ src/main/java/com/haoqiang/vpn/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.haoqiang.vpn.config;
 
+import com.haoqiang.vpn.extend.security.RestAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,18 +17,40 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    //step1:
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+//        auth.inMemoryAuthentication().withUser("user1")
+//                .password("{noop}password").roles("REGISTER_USER");
+//    }
+//
+//
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin();
+//    }
 
-    public void configureGlobal(@Autowired AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication().withUser("user1")
-                .password("{noop}password").roles("xxxx");
+    //step2:
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().withUser("user")
+                .password("{noop}password").roles("ADMIN");
     }
 
 
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
+        http.csrf().disable().authorizeRequests().antMatchers("/api/users","/api/user").permitAll()
                 .and()
-                .formLogin();
+                    .authorizeRequests().antMatchers("/api/**").hasAnyRole("REGISTER_USER","ADMIN")
+                .and()
+                .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint);
+        //                                                    =new RestAuthenticationEntryPoint()
+
     }
+
 }
---
+++ src/main/java/com/haoqiang/vpn/extend/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.haoqiang.vpn.extend.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Haoqiang Lyu
+ * @date 2019-06-19 12:08
+ */
+
+@Component("restAuthenticationEntryPoint")
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Unauthorized");
+    }
+}
