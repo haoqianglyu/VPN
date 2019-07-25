8867b96707c734bb6034527eca8d2d12ecb61d29
haoqianglyu <haoqianglyu@gmail.com>
2019-07-25 16:21:34 -0400

    create Storage and SQS bean

--- src/main/java/com/haoqiang/vpn/config/AppConfig.java
+++ src/main/java/com/haoqiang/vpn/config/AppConfig.java
@@ -1,15 +1,33 @@
 package com.haoqiang.vpn.config;
 
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.FilterType;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.haoqiang.vpn.service.StorageService;
+import org.springframework.context.annotation.*;
+
 
 @Configuration
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan(basePackages = "com.haoqiang.vpn",
         excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX,pattern = "com.haoqiang.vpn.api.*"))
 public class AppConfig {
+
+    @Bean
+    public StorageService getStorageServiceBean(){
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new DefaultAWSCredentialsProviderChain()).build();
+        StorageService ss = new StorageService(s3Client);
+        ss.setBucket("vpnlyu-dev");
+        return ss;
+    }
+
+    @Bean
+    public AmazonSQS getAmazonSQS(){
+        AmazonSQS client = AmazonSQSClientBuilder.standard().withCredentials(new DefaultAWSCredentialsProviderChain()).build();
+        return client;
+    }
 
 }
